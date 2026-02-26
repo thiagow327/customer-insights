bf62f8319edf48fe83a401ffb67accc03576121e
Gomes <thiagosilva327@live.com>
2026-02-25 21:51:33 -0300

    feat: add resource route for insights using InsightController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InsightController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('insights', InsightController::class);
