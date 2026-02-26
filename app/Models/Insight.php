5ac75d8f55dc7e5ea88d2e4e935b00fc9922fa8d
Gomes <thiagosilva327@live.com>
2026-02-25 21:48:48 -0300

    feat: define fillable properties in Insight model

--- app/Models/Insight.php
+++ app/Models/Insight.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Insight extends Model
 {
-    //
+    protected $fillable = [
+        'cliente',
+        'canal',
+        'sentimento',
+        'risco',
+        'problema',
+        'status',
+    ];
 }
